4e8254abe86d4f0d7b3d812a86372dfc78e03f97
Ed Norris <4132634+ed-norris@users.noreply.github.com>
2024-11-06 12:35:24 -0800

    Initial Commit

---
+++ Metronome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Metronome
+//
+//  Created by Ed Norris on 11/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by Ed Norris on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
